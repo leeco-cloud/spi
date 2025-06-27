e0dea19b400692fdb3c81b5b4fc9c9803bef1c75
lee <a6265391@qq.com>
2025-06-28 02:22:51 +0800

    init

---
+++ src/main/java/com/lee/spi/SpiApplication.java
@@ -0,0 +1,13 @@
+package com.lee.spi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiApplication.class, args);
+    }
+
+}
